5639c68d17e5007fd82f68262cc6901149f718f1
vijaybrutus <vijaybrutus@gmail.com>
2024-11-18 22:45:49 +0530

    content

--- src/components/CartContainer.tsx
+++ src/components/CartContainer.tsx
@@ -9,6 +9,7 @@ import { increaseQuantity, decreaseQuantity, resetCart } from '@/redux/shoppersS
 import CartItem from './CartItem';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import Button from './Button';
 
 const CartContainer = () => {
   const { cart } = useSelector((store: storeState) => store.shoppers);
@@ -40,7 +41,30 @@ const CartContainer = () => {
                font-semibold uppercase mb-4 hover:bg-red-600 
                hoverEffect rounded-md text-sm border-0'>Reset cart</button>
           </div>
-        </div> : <motion.div initial={{y:20, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:.4}} className='grid place-items-center py-20'>
+          <div className='grid place-items-end max-w-7xl'>
+            <div className='w-96 flex-col gap-4 items-center'>
+              <h1 className='font-bold text-2xl text-end py-3'>Cart Totals</h1>
+              <div>
+                <p className='flex items-center justify-between
+                 border-[1px] border-gray-400 border-b-0 py-1.5 px-4
+                 text-lg font-medium'>Subtotal <FormatedPrice amount={250} /></p>
+              </div>
+              <div>
+                <p className='flex items-center justify-between
+                 border-[1px] border-gray-400 border-b-0 py-1.5 px-4
+                 text-lg font-medium'>Shipping Charge <FormatedPrice amount={250} /></p>
+              </div>
+              <div>
+                <p className='flex items-center justify-between
+                 border-[1px] border-gray-400 py-1.5 px-4
+                 text-lg font-medium'>Total <FormatedPrice amount={250} /></p>
+              </div>
+            </div>
+            <div className='w-96 py-3 px-8 flex items-center'>
+              <Button name='Proceed to Checkout' className='w-96 py-3 px-8 rounded-md mt-4  hover:text-white' />
+            </div>
+          </div>
+        </div> : <motion.div initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: .4 }} className='grid place-items-center py-20'>
           <div className='max-w-[500px] px-4 py-8 bg-white flex flex-col gap-4 items-center rounded-md shadow-lg'>
             <h1 className='text-xl font-bold uppercase'>Your cart feels lonely</h1>
             <p className='text-sm text-center px-10 mt-2'>
